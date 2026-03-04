4cf0c421081585a22631bc01c4cdf6737ed0945b
Ramazan <rmznayaz0721mh@gmail.com>
2026-03-04 11:09:15 +0300

    girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan program practice commited.

--- java101/Practices/src/TekSayi.java
+++ java101/Practices/src/TekSayi.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class TekSayiToplayici {
+public class TekSayi {
     public static void main(String[] args) {
         
+        // Değişkenleri tanımla
         int sayi, toplam = 0;
 
+        // Kullanıcıdan sayı bilgisini al
         Scanner inp = new Scanner(System.in);
         System.out.print("Sayı giriniz: ");
         sayi = inp.nextInt();
 
+        // Kullanıcı tek sayı girdiği sürece döngüyü devam ettir
         do {
 
+            // Eğer sayı çift ve 4'ün katı ise toplamı güncelle
             if (sayi % 2 == 0 && sayi % 4 == 0) {
                 toplam += sayi;
             }
@@ -18,6 +22,7 @@ public class TekSayiToplayici {
 
         } while (sayi % 2 == 0);
         
+        // 4'ün katları toplamını ekrana yazdır
         System.out.println("4'ün katları toplamı: " + toplam);
     }
 }
