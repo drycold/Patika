78a83bc616bad0a54fdbc60b6ae11f73148763d8
Ramazan <rmznayaz0721mh@gmail.com>
2026-03-04 11:45:22 +0300

    girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran program practice commited.

---
+++ java101/Practices/src/SayininKuvveti.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SayininKuvveti {
+    public static void main(String[] args) {
+        
+        // Değişkenleri tanımla
+        int sayi;
+
+        // Kullanıcıdan sayı bilgisini al
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        sayi = inp.nextInt();
+        
+        // 4'ün ve 5'in kuvvetlerini ekrana yazdır
+        System.out.print("4'ün kuvvetleri: ");
+        for (int i = 4; i <= sayi; i*=4) {
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\n5'in kuvvetleri: ");
+        for (int j = 5; j <= sayi; j*=5) {
+            System.out.print(j + " ");
+        }
+    }
+}
